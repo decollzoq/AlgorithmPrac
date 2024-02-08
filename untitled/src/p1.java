135b5158b754f076f476baa7f8f54b2e0ff4f882
decollzoq <decollzoq@gmail.com>
2024-02-08 14:46:21 +0900

    문제 001

---
+++ untitled/src/p1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Hello {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    String sNum = sc.next();
+    char[] cNum = sNum.toCharArray();
+    int sum = 0;
+
+    for(int i = 0; i < N; i++){
+        
+    }
+}
