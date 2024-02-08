a6936a21d8f32383b2ecbffecec2743a1460b52a
decollzoq <decollzoq@gmail.com>
2024-02-08 16:00:55 +0900

    문제 003 BufferedReader 와 StringTokenizer 이용하여 풀이

--- practice/src/p3.java
+++ practice/src/p3.java
@@ -1,31 +1,33 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class p3 {
     // 백준 p11659
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int query = sc.nextInt();
-        long[] iNums = new long[N];
-        long[] iSums = new long[N];
-        iNums[0] = sc.nextLong();
-        iSums[0] = iNums[0];
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        for(int i = 1; i < N; i++){
-            iNums[i] = sc.nextLong();
-            iSums[i] = iSums[i-1] + iNums[i];
+        /* StringTokenizer : 문자열을 delim(delimiter) 으로 나눠 저장하는 것
+            토큰 : 쪼개어진 문자
+            delim : 디폴트로는 \n, \t, \r 등이 있고, 직접 정할 수도 있다.
+         */
+
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int numsNo = Integer.parseInt(stringTokenizer.nextToken());
+        int quizNo = Integer.parseInt(stringTokenizer.nextToken());
+        long[] iSums = new long[numsNo + 1];
+
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        for(int i = 1; i <= numsNo; i++){
+            iSums[i] = iSums[i-1] + Integer.parseInt(stringTokenizer.nextToken());
         }
 
-        for(int i = 0; i < query; i++){
-            int start = sc.nextInt() - 1;
-            int end = sc.nextInt() - 1;
-            if(start == end) {
-                System.out.println(iNums[end]);
-                continue;
-            }
-            if(start == 0) {
-                System.out.println(iSums[end]);
-                continue;
-            }
+        for(int i = 0; i < quizNo; i++){
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int start = Integer.parseInt(stringTokenizer.nextToken());
+            int end = Integer.parseInt(stringTokenizer.nextToken());
             System.out.println(iSums[end] - iSums[start-1]);
         }
     }
