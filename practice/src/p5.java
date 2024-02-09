2e1db1b52fbaf77addd4e1d510bf85f5b55ffcbd
decollzoq <decollzoq@gmail.com>
2024-02-09 10:38:12 +0900

    문제 005 변수의 자료형을 주의하자

--- practice/src/p5.java
+++ practice/src/p5.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class p5 {
+import java.sql.SQLNonTransientConnectionException;
+import java.util.Scanner;
+public class p5 {
+    // 백준 p10986
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        long cnt = 0;
+
+        long[] S = new long[N]; // 합 배열
+        long[] D = new long[M]; // D[i] = 나머지가 i인 합의 개수
+
+        S[0] = sc.nextInt();
+        for(int i = 1; i < N; i++){
+            S[i] = S[i-1] + sc.nextInt();
+        }
+        for(int i = 0; i < N; i++){
+            int remainder = (int)(S[i] % M);    // 0 <= remainder < 1000
+            D[remainder]++;
+        }
+
+        cnt += D[0];
+
+        for(int i = 0; i < M; i++){
+            if(D[i] > 1){
+                cnt += D[i] * (D[i]-1) / 2; // 오버플로우 발생 할 수 있으므로 cnt 타입은 long
+            }
+        }
+
+        System.out.print(cnt);
+    }
+
 }
