86aa1fb23faf6e8c7fb428c7bacdfd256294aa39
decollzoq <decollzoq@gmail.com>
2024-02-10 23:10:23 +0900

    문제 006

---
+++ practice/src/p6.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class p6 {
+    public static void main(String[] args){
+        int N = new Scanner(System.in).nextInt();
+        int start = 1;
+        int end = 1;
+        int sum = 1;
+        int count = 1;
+        while(end != N){
+            if(sum == N){
+                count++;
+                end++;
+                sum += end;
+            }
+            else if(sum > N){
+                sum -= start;
+                start++;
+            }
+            else if(sum < N) {
+                end++;
+                sum += end;
+            }
+        }
+        System.out.println(count);
+    }
+}
