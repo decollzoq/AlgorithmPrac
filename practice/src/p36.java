23f00ee412bccf6ca15a3f6e46f647cee4c87431
decollzoq <decollzoq@gmail.com>
2024-02-15 19:38:08 +0900

    문제 036

---
+++ practice/src/p36.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class p36 {
+    // 백준 p1541
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String data = sc.next();
+        String[] str = data.split("-");
+        int ans = 0;
+        for(int i = 0; i < str.length; i++){
+            int sum = add(str[i]);
+            if(i == 0) ans += sum;
+            else ans -= sum;
+        }
+        System.out.println(ans);
+    }
+
+    public static int add(String str){
+        String[] arr = str.split("[+]");
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            sum += Integer.parseInt(arr[i]);
+        }
+        return sum;
+    }
+}
