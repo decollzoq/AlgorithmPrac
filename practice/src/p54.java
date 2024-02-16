843732018842324904722a315e9da8361566d8f3
decollzoq <decollzoq@gmail.com>
2024-02-16 12:52:25 +0900

    Queue import

--- practice/src/p54.java
+++ practice/src/p54.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class p54 {
